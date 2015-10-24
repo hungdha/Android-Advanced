6598f38a66e188f7665593ed89a86f0007256b01
Le Hung <tuanhung.cntt@gmail.com>
2015-10-24 09:52:25 +0700

    + Add class extend of contact

---
+++ app/src/main/java/hungle/com/sqliteandroidlession/ContactNote.java
@@ -0,0 +1,7 @@
+package hungle.com.sqliteandroidlession;
+
+/**
+ * Created by Le Hung on 10/24/2015 9:49 AM.
+ */
+public class ContactNote {
+}
---
+++ app/src/main/java/hungle/com/sqliteandroidlession/ContactProfile.java
@@ -0,0 +1,7 @@
+package hungle.com.sqliteandroidlession;
+
+/**
+ * Created by Le Hung on 10/24/2015 9:48 AM.
+ */
+public class ContactProfile {
+}
